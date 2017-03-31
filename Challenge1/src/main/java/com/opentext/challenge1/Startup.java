264e5357f7d9440f007fab95bfda4e37054789b2
ojesvi <ojesvikushwah@gmail.com>
2017-03-31 14:57:10 +0200

    Update Startup.java

--- Challenge1/src/main/java/com/opentext/challenge1/Startup.java
+++ Challenge1/src/main/java/com/opentext/challenge1/Startup.java
@@ -1,4 +1,4 @@
-package com.mediasaturn.challenge1;
+package com.opentext.challenge1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
